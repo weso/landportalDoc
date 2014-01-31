4fb9aba7d2aa9f8e52b4ded92df9a32752045cd6
labra <jelabra@gmail.com>
2014-01-31 08:14:26 +0100

    New version

--- model/domain/tocTab.js
+++ model/domain/tocTab.js
@@ -1,3 +1,3 @@
 var tocTab = new Array();
-tocTab[0] = new Array ("0", "Domain Model", "EARoot/contents.html", "");
+tocTab[0] = new Array ("0", "System View", "EARoot/contents.html", "");
 var nCols = 20;
