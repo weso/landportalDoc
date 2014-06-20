1dcabd1236a704bb124839066be18266646a083d
DaniFdezAlvarez <danifdezalvarez@gmail.com>
2014-06-20 13:46:21 +0200

    Put diagrams up to date

--- model/domain/tocTab.js
+++
@@ -1,3 +0,0 @@
-var tocTab = new Array();
-tocTab[0] = new Array ("0", "Domain Model", "EARoot/contents.html", "");
-var nCols = 20;
--- model/system/tocTab.js
+++
@@ -1,3 +0,0 @@
-var tocTab = new Array();
-tocTab[0] = new Array ("0", "System View", "EARoot/contents.html", "");
-var nCols = 20;
